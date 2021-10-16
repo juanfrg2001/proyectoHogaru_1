eda55924d3f52fcbabea020ce6a36464ffcc4696
juanf <jfruizg@unbosque.edu.co>
2021-10-16 15:45:09 -0500

    Header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'proyecto/index_proyecto'
+  root 'proyecto#index_proyecto'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
