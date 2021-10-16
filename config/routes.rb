691cf30fa55b9988a5834ab9745225394e4f9641
juanf <jfruizg@unbosque.edu.co>
2021-10-16 16:00:30 -0500

    Header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'prueba_heroku2/index_prueba'
   root 'proyecto#index_proyecto'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
