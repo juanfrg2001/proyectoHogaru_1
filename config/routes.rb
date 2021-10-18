13d80999f66e124f26274172f2dc4f6c5b5185cc
juanf <jfruizg@unbosque.edu.co>
2021-10-18 12:54:08 -0500

    Update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/Ruby'
-  get 'static_pages/git'
+  get 'home', to: 'static_pages#home'
+  get 'basics', to: 'static_pages#basics'
+  get 'ruby', to: 'static_pages#ruby'
+  get 'git', to: 'static_pages#git'
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
